efc34d68beebf66e075bc90aeef1f2a3bc0b52b7
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2025-06-07 22:23:25 +0500

    2. Select Statement

---
+++ 2. Select Statment.sql
@@ -0,0 +1,24 @@
+SELECT * FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name,
+age,
+(age + 10)
+FROM parks_and_recreation.employee_demographics;
+
+#PEMDAS
+
+SELECT distinct gender
+FROM parks_and_recreation.employee_demographics;
+
+SELECT distinct gender,first_name
+FROM parks_and_recreation.employee_demographics;
+
+
+
+
+
+
+
+
+
+
